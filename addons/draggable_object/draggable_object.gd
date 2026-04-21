657c636c0bfbececedb5651f83b21e4cc4918ac3
Andrés López <andreslopez01182009@gmail.com>
2026-04-21 15:23:56 -0500

    Corrección en animaciones y efectos extra
    Probablemente el final de esta rama

--- addons/draggable_object/draggable_object.gd
+++ addons/draggable_object/draggable_object.gd
@@ -6,8 +6,12 @@
 class_name DraggableObject extends PhysicsBody2D
 
 
+## Indica que el objeto inicia o termina su arrastre
+signal dragging(value: bool)
+
+
 ## Velocidad a la que se mueve el objeto
-const DRAG_LERP_TIME: float = 0.1
+const DRAG_LERP_SPEED: float = 10
 
 
 ## Permite que el objeto sea arrastable con el mouse
@@ -17,18 +21,19 @@ const DRAG_LERP_TIME: float = 0.1
 ## Indica si se esta arrastrando el objeto actualmente
 var is_dragging: bool = false:
 	set(value):
+		if is_dragging == value: return
 		is_dragging = value
+		dragging.emit(value) # Emitimos solo cuando el estado cambia
 		print("[DraggableObject] Dragging is %s" % value)
 
 
 ## Maneja el arrastre del objeto con el mouse. Debes incluirlo en el método [code]physics_process[/code]
-func handle_dragging() -> void:
+func handle_dragging(delta: float) -> void:
 	if not draggable or not is_dragging: return
+	if global_position.distance_squared_to(get_global_mouse_position()) < 0.1: return
 
-	# Suaviza el movimiento del objeto
-	var tween := create_tween()
-	tween.tween_property(self, "global_position", get_global_mouse_position(), DRAG_LERP_TIME)
-	global_position = get_global_mouse_position()
+	# Para un arrastre suave, lerp es más eficiente
+	global_position = global_position.lerp(get_global_mouse_position(), DRAG_LERP_SPEED * delta)
 
 
 func _input_event(_viewport, event: InputEvent, _shape_idx) -> void:
--- assets/graph_node/graphim_node.gd
+++ assets/graph_node/graphim_node.gd
@@ -5,7 +5,7 @@ class_name GraphimNode extends DraggableObject
 ## Escala al rebotar el nodo
 const BUMP_SCALE := 1.1
 ## Tiempo de rebote
-const BUMP_TIME := 0.2
+const DRAG_EFFECT_TIME := 0.2
 
 
 ## Color del nodo
@@ -13,6 +13,7 @@ const BUMP_TIME := 0.2
 	set(value):
 		if not is_node_ready(): await ready
 		color = value
+		_bump()
 		_set_color(value)
 
 
@@ -23,6 +24,10 @@ var velocity: Vector2
 var last_global_pos := Vector2.ZERO
 
 
+func _ready() -> void:
+	dragging.connect(_set_drag_visuals)
+
+
 func _physics_process(delta: float) -> void:
 	var distance := global_position - last_global_pos
 	last_global_pos = global_position
@@ -30,13 +35,33 @@ func _physics_process(delta: float) -> void:
 	velocity = distance / delta
 
 	# Necesario para que funcione el arrastre con el mouse
-	handle_dragging()
+	handle_dragging(delta)
+
+
+#region Visuales
 
 
 ## Establece el color del nodo
-func _set_color(_color: Color) -> void:
-	_bump()
-	sprite.modulate = _color
+func _set_color(_color: Color, tweened := false) -> void:
+	if not tweened:
+		sprite.modulate = _color
+		return
+
+	var tween := create_tween().set_trans(Tween.TRANS_SINE)
+	tween.tween_property(sprite, "modulate", _color, DRAG_EFFECT_TIME)
+
+
+## Establece el efecto visual al arrastrar el objeto
+func _set_drag_visuals(value: bool) -> void:
+	if value:
+		_expand()
+		_set_color(Color.GRAY, true)
+	else:
+		_contract()
+		_set_color(Color.WHITE, true)
+
+
+#endregion
 
 
 #region Tamaño del nodo
@@ -44,20 +69,22 @@ func _set_color(_color: Color) -> void:
 
 ## Aumenta el tamaño del nodo y lo deja como antes
 func _bump() -> void:
-	_expand()
+	await _expand().finished
 	_contract()
 
 
-## Expande el nodo
-func _expand():
+## Expande el nodo. Usa el tween que retorna para detectar el final
+func _expand() -> Tween:
 	var tween := create_tween().set_trans(Tween.TRANS_SINE)
-	tween.tween_property(sprite, "scale", Vector2(BUMP_SCALE, BUMP_SCALE), BUMP_TIME)
+	tween.tween_property(sprite, "scale", Vector2(BUMP_SCALE, BUMP_SCALE), DRAG_EFFECT_TIME)
+	return tween
 
 
-## Contrae el nodo a su tamaño original
-func _contract():
+## Contrae el nodo a su tamaño original. Usa el tween que retorna para detectar el final
+func _contract() -> Tween:
 	var tween := create_tween().set_trans(Tween.TRANS_SINE)
-	tween.tween_property(sprite, "scale", Vector2(1, 1), BUMP_TIME)
+	tween.tween_property(sprite, "scale", Vector2(1, 1), DRAG_EFFECT_TIME)
+	return tween
 
 
 #endregion
