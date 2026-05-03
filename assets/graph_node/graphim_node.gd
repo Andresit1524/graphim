0acd965b3144b5337c2b5a7d87d69582f7cbc564
Andrés López <andreslopez01182009@gmail.com>
2026-05-02 19:07:27 -0500

    Prototipo de repulsión entre nodos y bordes de pantalla

--- assets/graph_node/graphim_node.gd
+++ assets/graph_node/graphim_node.gd
@@ -2,14 +2,24 @@
 class_name GraphimNode extends DraggableObject
 
 
+## Fuerza de repulsión. Constante de Coulomb modificada
+@export var repulsion_force: float = 9e7
+## Fricción del movimiento
+@export var damping: float = 0.1
+
+
 @onready var sprite: Sprite2D = $Sprite
 @onready var label: Label = %Label
 @onready var data: NodeData = $Data
+@onready var last_global_pos := global_position
 
 
-# Velocidad y última posición
-var velocity: Vector2
-var last_global_pos := Vector2.ZERO
+## Desactiva el nodo y sus físicas. Usado para el DragInstancer
+var disabled := false:
+	set(value):
+		disabled = value
+		input_pickable = not value
+		if not value: _reset_physics()
 
 
 func _ready() -> void:
@@ -18,15 +28,16 @@ func _ready() -> void:
 	data.weight_changed.connect(_set_weight)
 	data.color_changed.connect(_set_color)
 
-	# Fuerza a actualiizar
+	# Fuerza a actualiizar los datos
 	data.refresh()
 
 
 func _physics_process(delta: float) -> void:
-	var distance := global_position - last_global_pos
-	last_global_pos = global_position
+	var current_pos := global_position
 
-	velocity = distance / delta
+	# Aplica la fuerza sobre si mísmo y actualiza la posición
+	_apply_force(delta)
+	last_global_pos = current_pos
 
 	# Necesario para que funcione el arrastre con el mouse
 	handle_dragging(delta)
@@ -89,6 +100,43 @@ func _contract() -> Tween:
 	var tween := create_tween().set_trans(Tween.TRANS_SINE)
 	tween.tween_property(sprite, "scale", Vector2(1, 1), Constants.EFFECT_TIME)
 	return tween
+
+
+#endregion
+
+
+#region Físicas
+
+
+## Resetea la física del objeto (util al instanciar)
+func _reset_physics() -> void:
+	last_global_pos = global_position
+
+
+## Aplica las fuerzas sobre el objeto
+func _apply_force(delta: float) -> void:
+	if disabled: return
+
+	# Calculamos la sumatoria de cada repulsión de los otros nodos
+	var force := Vector2.ZERO
+	for node: GraphimNode in get_tree().get_nodes_in_group(&"nodes"):
+		if node == self or node.disabled: continue
+		force += _coulomb(node.global_position)
+
+	# Aplica Verlet con fricción sobre la velocidad (el desplazamiento)
+	var inertia := (global_position - last_global_pos) * (1.0 - damping)
+	var displacement := inertia + (force * delta * delta)
+
+	# Usamos el motor de Godot para mover y detenernos si hay colisión
+	move_and_collide(displacement)
+
+
+## Calcula la repulsión usando la ley de Coulomb para un punto dado (posición global)
+func _coulomb(point_pos: Vector2) -> Vector2:
+	var distance := global_position - point_pos
+
+	# Evitamos división por cero y suavizamos la fuerza en distancias cortas (+ 100)
+	return distance.normalized() * repulsion_force / (distance.length_squared() + 100.0)
 
 
 #endregion
