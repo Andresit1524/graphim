c615a01cf6f6bf107e9062d1bfe24227634a4d9d
Andrés López <andreslopez01182009@gmail.com>
2026-05-10 19:52:52 -0500

    Lista de nodos

--- test/world.gd
+++ test/world.gd
@@ -3,11 +3,25 @@ class_name World extends Node2D
 
 ## Lista de botones
 @onready var buttons: Buttons = %Buttons
+## Texto de lista de nodos
+@onready var list: RichTextLabel = %Text
 
 ## Lista de aristas
 @onready var edges: Node2D = %Edges
 ## Lista de nodos del grafo
 @onready var nodes: Node2D = %Nodes
+
+
+## Nombres de los nodos
+var nodes_names: String:
+	set(value):
+		nodes_names = value
+		_update_list()
+## Nombres de las aristas
+var edges_names: String:
+	set(value):
+		edges_names = value
+		_update_list()
 
 
 func _ready() -> void:
@@ -38,13 +52,36 @@ func _delete_graph() -> void:
 ## Actualiza la lista de nodos
 func _update_nodes() -> void:
 	print("[World] Nodos actualizados")
+	nodes_names = _nodes_to_string(nodes.get_children())
+
 	# TODO: implementación en el grafo abstracto
 
 
 ## Actualiza la lista de aristas
 func _update_edges() -> void:
 	print("[World] Aristas actualizadas")
+	edges_names = _nodes_to_string(edges.get_children())
+
 	# TODO: implementación en el grafo abstracto
+
+
+## Auxiliar: convierte la lista de nodos en una lista
+func _nodes_to_string(nodes_list: Array[Node]) -> String:
+	return nodes_list.reduce(func(current_text: String, node: Node):
+		return (current_text + "\n- %s" % node.name),
+		""
+	)
+
+
+#endregion
+
+
+#region Interfaz
+
+
+## Actualiza la lista de textos
+func _update_list() -> void:
+	list.text = "[center][b]Nodes[/b][/center]" + nodes_names + "\n\n[center][b]Aristas[/b][/center]" + edges_names
 
 
 #endregion
