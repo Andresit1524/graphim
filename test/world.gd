55d0ac7ab3e76f36cc33755d631b7414e9e41a73
Andrés López <andreslopez01182009@gmail.com>
2026-05-10 19:28:02 -0500

    Actualización de nodos y aristas
    Falta la implementación

--- test/world.gd
+++ test/world.gd
@@ -5,13 +5,25 @@ class_name World extends Node2D
 @onready var buttons: Buttons = %Buttons
 
 ## Lista de aristas
-@onready var edges: Node2D = $Edges
+@onready var edges: Node2D = %Edges
 ## Lista de nodos del grafo
-@onready var nodes: Node2D = $Nodes
+@onready var nodes: Node2D = %Nodes
 
 
 func _ready() -> void:
 	buttons.delete_graph.connect(_delete_graph)
+	# TODO: Resto de señales de los botones
+
+	# Conecta la lista de nodos y de aristas
+	edges.child_order_changed.connect(_update_edges)
+	nodes.child_order_changed.connect(_update_nodes)
+
+	# Actualiza a la fuerza
+	_update_edges()
+	_update_nodes()
+
+
+#region Manejo del grafo
 
 
 ## Elimina el grafo actual
@@ -23,7 +35,28 @@ func _delete_graph() -> void:
 		child.queue_free()
 
 
+## Actualiza la lista de nodos
+func _update_nodes() -> void:
+	print("[World] Nodos actualizados")
+	# TODO: implementación en el grafo abstracto
+
+
+## Actualiza la lista de aristas
+func _update_edges() -> void:
+	print("[World] Aristas actualizadas")
+	# TODO: implementación en el grafo abstracto
+
+
+#endregion
+
+
+#region Entrada
+
+
 func _unhandled_input(event: InputEvent) -> void:
 	# Resetea el botón de borrar si se clica por fuera
 	if event is InputEventMouseButton and event.is_pressed():
 		buttons.has_delete_button_pressed = false
+
+
+#endregion
