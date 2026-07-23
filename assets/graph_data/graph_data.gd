d77db1b27da0930a280774b54144a50840c59c41
Andrés López <andreslopez01182009@gmail.com>
2026-07-23 14:52:22 -0500

    Corrección para cálculo de adyacencias GraphData
    Ahora si sigue una lógica correcta

--- assets/graph_data/graph_data.gd
+++ assets/graph_data/graph_data.gd
@@ -1,49 +1,55 @@
-## [code]GraphData[/code] contiene los datos de una grafo, de forma serializable y persistente.
+## Contiene los datos de un grafo (no referencias), de forma serializable y persistente
 class_name GraphData extends Resource
 
 
-## Lista de nodos
-@export var nodes: Array
+## Lista de datos de nodos
+@export var nodes_data: Array
 
-## Lista de aristas
-@export var edges: Array
+## Lista de datos de aristas
+@export var edges_data: Array
 
 
 ## Obtiene la lista de adyacencia del grafo actual
-func get_adyacency_list() -> Dictionary[GraphimNode, Array]:
+# ? Se usará para los algoritmos, pero eso es más adelante
+func get_adyacency_list() -> Dictionary[NodeData, Array]:
 	var adyacency_list := {}
-	for node: GraphimNode in nodes:
-		adyacency_list[node] = edges.filter(
-			func(e: GraphimEdge): return e.data.start_node == node
-		).map(
-			func(e: GraphimEdge): return e.data.end_node
-		)
+	var nodes_by_uid := {}
+
+	# Registra por UID
+	for n in nodes_data:
+		nodes_by_uid[n.uid] = n
+		adyacency_list[n] = []
+
+	# Filtra
+	for e in edges_data:
+		var start: NodeData = nodes_by_uid.get(e.start_uid)
+		var end: NodeData = nodes_by_uid.get(e.end_uid)
+		if start and end: adyacency_list[start].append(end)
 
 	return adyacency_list
 
 
-## Obtiene la matriz de adyacencia del grafo actual.
+## Obtiene la matriz de adyacencia (en producto cartesiano) del grafo actual.
 ## Formato: [inicio, final]: peso. Si no existe, el peso es NAN
+# ? Se usará para los algoritmos, pero eso es más adelante
 func get_adyacency_matrix() -> Dictionary[Array, float]:
 	var adyacency_matrix := {}
+	var nodes_by_uid := {}
+
+	# Registra por UID
+	for n in nodes_data:
+		nodes_by_uid[n.uid] = n
 
 	# Primera pasada para las aristas
-	for node_a: GraphimNode in nodes:
-		for node_b: GraphimNode in nodes:
-			adyacency_matrix[[node_a, node_b]] = edges.filter(
-				func(e: GraphimEdge): return (
-					e.data.start_node == node_a
-					and e.data.end_node == node_b
-				)
-			).map(
-				func(e: GraphimEdge): return e.data.weight
-			)
+	for e in edges_data:
+		var start: NodeData = nodes_by_uid.get(e.start_uid)
+		var end: NodeData = nodes_by_uid.get(e.end_uid)
+		if start and end: adyacency_matrix[[start, end]] = e.weight
 
 	# Segunda pasada para el resto
-	for node_a: GraphimNode in nodes:
-		for node_b: GraphimNode in nodes:
+	for node_a: NodeData in nodes_data:
+		for node_b: NodeData in nodes_data:
 			if adyacency_matrix.has([node_a, node_b]): continue
-
 			adyacency_matrix[[node_a, node_b]] = NAN
 
 	return adyacency_matrix
